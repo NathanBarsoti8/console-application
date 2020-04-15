8b937dc5923f47b712665a18e52c4a27663a3d59
Nathan Barsoti <nathan.barsoti@programmers.com.br>
2020-04-15 20:33:59 -0300

    added the base of receive proj

---
+++ Receive/Receive/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Receive
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            MainClass main = new MainClass();
+            TcpListener server = null;
+            TcpClient client = null;
+            NetworkStream stream = null;
+
+            Byte[] bytes = new Byte[701264];
+            string data = null;
+
+            try
+            {
+                Int32 port = 5000;
+                IPAddress local = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(local, port);
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Trying to connect...");
+                    client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected");
+                    data = null;
+                    stream = client.GetStream();
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        //Translate data bytes to a ASCII string
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        data = data.ToUpper();
+
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (WebException exc)
+            {
+                Console.WriteLine("Connect failed", exc);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+        }
+    }
+}
--- Send/Send/Program.cs
+++ Send/Send/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -12,55 +13,31 @@ namespace Send
         {
             NetworkStream stream = null;
 
+            try
+            {
+                Int32 port = 5000;
+                TcpClient client = new TcpClient("127.0.0.1", port);
 
-            Int32 port = 110000;
-            TcpClient client = new TcpClient("127.0.0.1", port);
+                Console.WriteLine("Cliente ------> Trying to connect, wait...");
+                Console.WriteLine("Reading the doc, wait...");
 
+                string text2 = File.ReadAllText(@"C:\Users\nathan.barsoti\Desktop\application\console-application\access (1).log");
 
-            //string[] text = File.ReadAllLines(@"C:\Users\nathan.barsoti\Desktop\application\console-application\access (1).log");
-            
-            //string formatPattern = (@"(?<date>\d{10}.\d+)(\s{5}|\s{4})\d+(\s(?<ip>\d{3}\.\d{3}\.\d{2}\.(\d{3}|\d{2}))(?:[^h]*)(?<domain>[^\s]+))");
+                Byte[] data = Encoding.ASCII.GetBytes(text2);
+                stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
 
-            //Regex rgx = new Regex(formatPattern);
+                Console.WriteLine("Sent: {0}", data);
+                Console.WriteLine("Size byte {0} in data send", data.Length);
 
-            //for (int i = 0; i < text.Length; i++)
-            //{
-            //    if (rgx.IsMatch(text[i]))
-            //    {
-            //        var domain = rgx.Match(text[i]).Groups[6].Value;
-            //        var ip = rgx.Match(text[i]).Groups[5].Value;
-            //        var date = rgx.Match(text[i]).Groups[4].Value;
+                stream.Close();
+                client.Close();
+                Console.ReadKey();
+            }
 
-            //        double parseDouble = Double.Parse(date);
-            //        DateTime r_date = new DateTime().AddSeconds(parseDouble);
-            //    }
-            //}
-
-            string text2 = File.ReadAllText(@"C:\Users\nathan.barsoti\Desktop\application\console-application\access (1).log");
-
-            Byte[] data = Encoding.ASCII.GetBytes(text2);
-            stream = client.GetStream();
-            stream.Write(data, 0, data.Length);
-
-            Console.WriteLine("Sent: {0}", data);
-            Console.WriteLine("Size bye {0} in data send", data.Length);
-
-            data = new Byte[data.Length];
-            string responseData = String.Empty;
-            Int32 bytes = stream.Read(data, 0, data.Length);
-            responseData = Encoding.ASCII.GetString(data, 0, bytes);
-            Console.WriteLine("Received: {0}", responseData);
-
-            stream.Close();
-            client.Close();
-            Console.ReadKey();
+            catch (WebException exc)
+            {Console.WriteLine("Failed ---> SEND", exc);}
         }
 
-        //public struct DataToSend
-        //{
-        //    public DateTime Date { get; set; }
-        //    public string Ip { get; set; }
-        //    public string Domain { get; set; }
-        //}
     }
 }
