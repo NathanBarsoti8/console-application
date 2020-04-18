ffafc1f19f273491f7d8cfe5fffed5322d697c38
Nathan Barsoti <nathan.barsoti@programmers.com.br>
2020-04-18 15:37:26 -0300

    fixed the client application; working in correct way

--- Send/Send/Program.cs
+++ Send/Send/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -17,18 +19,35 @@ namespace Send
             {
                 Int32 port = 5000;
                 TcpClient client = new TcpClient("127.0.0.1", port);
-
-                Console.WriteLine("Cliente ------> Trying to connect, wait...");
                 Console.WriteLine("Reading the doc, wait...");
 
-                string text2 = File.ReadAllText(@"C:\Users\nathan.barsoti\Desktop\application\console-application\access (1).log");
+                string[] text = File.ReadAllLines(@"C:\Users\nathan.barsoti\Desktop\application\console-application\access (1).log");
+                string formatPattern = (@"(?<date>\d{10}.\d+)(\s{5}|\s{4})\d+(\s(?<ip>\d{3}\.\d{3}\.\d{2}\.(\d{3}|\d{2}))(?:[^h]*)(?<domain>[^\s]+))");
 
-                Byte[] data = Encoding.ASCII.GetBytes(text2);
-                stream = client.GetStream();
-                stream.Write(data, 0, data.Length);
+                Regex rgx = new Regex(formatPattern);
 
-                Console.WriteLine("Sent: {0}", data);
-                Console.WriteLine("Size byte {0} in data send", data.Length);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (rgx.IsMatch(text[i]))
+                    {
+                        var url = rgx.Match(text[i]).Groups[6].Value;
+                        var ip = rgx.Match(text[i]).Groups[5].Value;
+                        var date = rgx.Match(text[i]).Groups[4].Value;
+
+                        double parseDouble = Double.Parse(date);
+                        DateTime r_date = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(parseDouble);
+
+                        DataToSend send = new DataToSend();
+                        send.Date = r_date;
+                        send.Ip = ip;
+                        send.Url = url;
+
+                        var bytes = GetBytes(send);
+
+                        stream = client.GetStream();
+                        stream.Write(bytes, 0, bytes.Length);
+                    }
+                }
 
                 stream.Close();
                 client.Close();
@@ -36,7 +55,27 @@ namespace Send
             }
 
             catch (WebException exc)
-            {Console.WriteLine("Failed ---> SEND", exc);}
+            {Console.WriteLine("Failed when trying to send", exc);}
+        }
+
+        public static byte[] GetBytes(DataToSend send)
+        {
+            int size = Marshal.SizeOf(send);
+            byte[] arr = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(send, ptr, false);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+
+            return arr;
+        }
+
+        public struct DataToSend
+        {
+            public DateTime Date { get; set; }
+            public string Ip { get; set; }
+            public string Url { get; set; }
         }
 
     }
