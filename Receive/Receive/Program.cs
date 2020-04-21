2b9c43b1fee6c55eb9cc460fc2189a42e686e4a6
Nathan Barsoti <nathan.barsoti@programmers.com.br>
2020-04-21 10:39:18 -0300

    added the connection and method to insert in db using bulk

--- Receive/Receive/Program.cs
+++ Receive/Receive/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Receive
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            MainClass main = new MainClass();
             Int32 port = 11000;
             UdpClient server = new UdpClient(port);
             IPEndPoint groupEp = new IPEndPoint(IPAddress.Any, 0);
@@ -27,11 +31,14 @@ namespace Receive
                     Console.WriteLine(data.Ip);
                     Console.WriteLine(data.Date);
                     Console.WriteLine(data.Url);
-                    
-                    
 
+
+                    int i = 1;
                     Console.WriteLine("Finished opp");
+                    Console.WriteLine("count");
+                    Console.WriteLine(i++);
                     //client.Close();
+                    main.InsertInDb(data);
                 }
             }
             catch (WebException exc)
@@ -43,6 +50,49 @@ namespace Receive
                 server.Close();
             }
 
+        }
+
+        public void InsertInDb(DataToReceive data)
+        {
+            string connectionString = @"Data Source = N092\SQLEXPRESS;" +
+                                      "Integrated Security=true;" +
+                                      "Initial Catalog = APPLICATION;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    DataTable table = new DataTable();
+                    table.Columns.Add("Id", typeof(Guid));
+                    table.Columns.Add("Ip", typeof(string));
+                    table.Columns.Add("Date", typeof(DateTime));
+                    table.Columns.Add("Url", typeof(string));
+
+                    DataRow row = table.NewRow();
+
+                    row["Id"] = Guid.NewGuid();
+                    row["Ip"] = data.Ip;
+                    row["Date"] = data.Date;
+                    row["Url"] = data.Url;
+
+                    table.Rows.Add(row);
+                    
+                    SqlBulkCopy bulkCopy = new SqlBulkCopy(connection);
+                    bulkCopy.DestinationTableName = "Info";
+                    bulkCopy.ColumnMappings.Add("Ip", "Ip");
+                    bulkCopy.ColumnMappings.Add("Date", "Date");
+                    bulkCopy.ColumnMappings.Add("Url", "Url");
+
+                    connection.Open();
+                    bulkCopy.WriteToServer(table);
+                    connection.Close();
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("Error => {0}", exc);
+                }
+
+            }
         }
 
         public static DataToReceive FromBytes(byte[] arr)
